280693a4c34184d26d814b83f9c621269574e2e3
ishant017 <ishantchaudhary0123@gmail.com>
2026-07-18 15:19:37 +0530

    A very basic calculator,take user input and give answer

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+# This is a simple calculator program in Python that performs basic arithmetic operations.
+
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+
+print(f"The sum of {a} and {b} is: {a+b}")#For addition
+print(f"The difference of {a} and {b} is: {a-b}")#For subtraction
+print(f"The product of {a} and {b} is: {a*b}")#For multiplication
+print(f"The quotient of {a} and {b} is: {a/b}")#For division
+print(f"The remainder of {a} and {b} is: {a%b}")#For remainder
+print(f"The floor division of {a} and {b} is: {a//b}")#For floor division
